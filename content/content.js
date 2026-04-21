0a52eb137787e757b39402f6f4f36860874ff04f
LarsTwiNky <165326752+LarsTwiNky@users.noreply.github.com>
2026-04-21 13:54:02 +0000

    init commit - add basic feature with slider in popup

---
+++ content/content.js
@@ -0,0 +1,12 @@
+
+chrome.storage.local.get(["sliderValue"], (result) => {
+    const val = result.sliderValue ?? 50;
+
+    document.documentElement.style.setProperty("--slider-value", val + "%");
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.type ==="SLIDER_UPDATE"){
+            document.documentElement.style.setProperty("--slider-value", message.value + "%");
+    }
+});
